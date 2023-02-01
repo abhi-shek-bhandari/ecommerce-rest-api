15b53043ca295a769edc207c085d54c2f94e5e42
abhi-shek-bhandari <abhishekbhandari06@outlook.com>
2023-02-01 15:33:37 +0530

    project started and dependencies added

---
+++ src/main/java/com/masai/demo/PrdApplication.java
@@ -0,0 +1,13 @@
+package com.masai.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrdApplication.class, args);
+	}
+
+}
