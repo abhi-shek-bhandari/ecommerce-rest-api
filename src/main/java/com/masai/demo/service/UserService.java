45955fc89e37f05166edcc70ce50621c33761808
abhi-shek-bhandari <abhishekbhandari06@outlook.com>
2023-02-04 23:57:11 +0530

    many functionality added with address class

--- src/main/java/com/masai/demo/service/UserService.java
+++ src/main/java/com/masai/demo/service/UserService.java
@@ -1,19 +1,27 @@
 package com.masai.demo.service;
 
+import com.masai.demo.dto.AddressDto;
 import com.masai.demo.dto.UserDto;
+import com.masai.demo.exception.AddressException;
+import com.masai.demo.exception.UserException;
+import com.masai.demo.model.Address;
 import com.masai.demo.model.User;
 
 import java.util.List;
+import java.util.Set;
 
 public interface UserService {
     User createUser(UserDto userDto);
-    UserDto updateUserName(String Name, Integer userId);
+    UserDto updateUserFirstName(String FirstName, Integer userId);
+    UserDto updateUserLastName(String LastName, Integer userId);
     UserDto updateUserEmail(String email, Integer userId);
     UserDto updateUserPassword(String Password, Integer userId);
     UserDto findUser(Integer userId);
     List<User> getListOfAllUsers();
-<<<<<<< HEAD
     User deleteUser(Integer userId);
-=======
->>>>>>> 86f3e830b822613583154b7a4fb6995679c28ee9
+
+    UserDto addAddress(AddressDto addressDto, Integer userId)throws AddressException, UserException;
+    Address deleteAddress(Integer id)throws AddressException;
+    Set<AddressDto> getAllUserAddress(Integer userId)throws AddressException;
+    AddressDto updateAddressByAddressId(AddressDto addressDto, Integer id)throws AddressException;
 }
